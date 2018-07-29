c252ecd0278f7c83f075d8703962cd522c274673
debashisnath <41103885+debashisnath@users.noreply.github.com>
2018-07-29 22:38:40 +0530

    JS exercise

---
+++ JS/Exercise9.js
@@ -0,0 +1,8 @@
+var person = {
+    firstName : "Debashis",
+    lastName : "Nath",
+    age : 22
+};
+
+console.log(Object.getOwnPropertyNames(person));
+console.log(Object.getOwnPropertyNames(person));
